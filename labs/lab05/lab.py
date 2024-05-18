d87d1c443e095f77ee2b8c743d221fb2d9facdca
Mia Jerphagnon <mjerphagnon@ucsd.edu>
2024-05-18 14:20:32 -0700

    all my edits

--- labs/lab05/lab.py
+++ labs/lab05/lab.py
@@ -13,7 +13,7 @@ from scipy import stats
 
 
 def after_purchase():
-    ...
+    return ['NMAR', 'MD', 'MAR', 'MAR', 'MAR']
 
 
 # ---------------------------------------------------------------------
@@ -22,7 +22,7 @@ def after_purchase():
 
 
 def multiple_choice():
-    ...
+    return ['MAR', 'MAR', 'MD', 'NMAR', 'MCAR']
 
 
 # ---------------------------------------------------------------------
@@ -32,20 +32,30 @@ def multiple_choice():
 
 
 def first_round():
-    ...
+    return [0.143, 'NR']
 
 
 def second_round():
-    ...
+    return [0.022, 'R', 'D']
 
 
 # ---------------------------------------------------------------------
 # QUESTION 4
 # ---------------------------------------------------------------------
 
+from scipy.stats import ks_2samp
 
 def verify_child(heights):
-    ...
+    p_vals = pd.Series(index=heights.columns[2:], dtype=float)
+    
+    for child_column in heights.columns[2:]:
+        child_missing = heights[heights[child_column].isna()]['father']
+        child_not_missing = heights[~heights[child_column].isna()]['father']
+        
+        ks_stat, p_val = ks_2samp(child_missing, child_not_missing)
+        p_vals[child_column] = p_val
+
+    return p_vals
 
 
 # ---------------------------------------------------------------------
@@ -54,7 +64,10 @@ def verify_child(heights):
 
 
 def cond_single_imputation(new_heights):
-    ...
+    quartiles = pd.qcut(new_heights['father'], q=4)
+    mean_child_by_quartile = new_heights.groupby(quartiles)['child'].mean()
+    imputed_child = new_heights['child'].fillna(new_heights.groupby(quartiles)['child'].transform('mean'))
+    return imputed_child
 
 
 # ---------------------------------------------------------------------
@@ -63,11 +76,26 @@ def cond_single_imputation(new_heights):
 
 
 def quantitative_distribution(child, N):
-    ...
+    non_missing_child = child.dropna()
+    freq, bin_edges = np.histogram(non_missing_child, density=True)
+    choice_distr = []
+    for i in range(len(freq)):
+        prob = (bin_edges[i+1]-bin_edges[i]) * freq[i]
+        choice_distr.append(prob)
+    imp_vals = []
+    for i in range(N):
+        bin_index = np.random.choice(range(10), p=choice_distr)
+        imp_val = np.random.uniform(bin_edges[bin_index], bin_edges[bin_index+1])
+        imp_vals.append(imp_val)
+    imp_vals = np.array(imp_vals)
+    return imp_vals
 
 
 def impute_height_quant(child):
-    ...
+    num_missing = child.isna().sum()
+    full_child = child.copy()
+    full_child[full_child.isna()] = quantitative_distribution(child, num_missing)
+    return full_child
 
 
 # ---------------------------------------------------------------------
@@ -76,4 +104,4 @@ def impute_height_quant(child):
 
 
 def answers():
-    ...
+    return [1, 2, 2, 1], ['soundcloud.com/robots.txt', 'instagram.com/robots.txt']
--- projects/03-language_models/project.py
+++ projects/03-language_models/project.py
@@ -15,7 +15,32 @@ import time
 
 
 def get_book(url):
-    ...
+    # Check robots.txt for the pause length
+    robots_url = url[:url.rfind('/')] + '/robots.txt'
+    robots_response = requests.get(robots_url)
+    pause = 0.5  # Default pause in case robots.txt doesn't specify
+
+    if robots_response.ok:
+        robots_txt = robots_response.text
+        match = re.search(r'Delay: (\d+)', robots_txt)
+        if match:
+            pause = int(match.group(1))
+
+    # Pause to adhere to robots.txt policy
+    time.sleep(pause)
+
+    # Fetch the content of the book
+    corpus = requests.get(url)
+    corpus_text = corpus.text
+
+    # Extract the contents without the start and end comments
+    corpus_text = corpus_text.split('***')[2:-2]
+    corpus_text = ''.join(corpus_text)
+
+    # Replace Windows newline characters with standard newline characters
+    corpus_text = corpus_text.replace('\r\n', '\n')
+
+    return corpus_text
 
 
 # ---------------------------------------------------------------------
@@ -24,7 +49,12 @@ def get_book(url):
 
 
 def tokenize(book_string):
-    ...
+    book_string = re.sub(r'\n{2,}', '\x02', book_string)
+    book_string = book_string.replace('\x02', ' \x03 \x02 ')
+    tokens = re.findall(r'\b\w+\b|\W', book_string)
+    tokens = list(filter(lambda x: x != ' ', tokens))
+    tokens = tokens[1:-1]
+    return tokens
 
 
 # ---------------------------------------------------------------------
@@ -34,19 +64,30 @@ def tokenize(book_string):
 
 class UniformLM(object):
 
-
     def __init__(self, tokens):
-
         self.mdl = self.train(tokens)
         
     def train(self, tokens):
-        ...
+        tokens_index = list(pd.Series(tokens).unique())
+        tokens_length = pd.Series(tokens).nunique()
+        tokens_values = [1/tokens_length] * tokens_length
+        tokens_series = city_series = pd.Series(tokens_values, index=tokens_index)
+        return tokens_series
     
     def probability(self, words):
-        ...
+        count = 0
+        for word in words:
+            if word in (self.mdl.index.to_list()):
+                count += 1
+        if count==0:
+            return count
+        else: 
+            return (self.mdl.iloc[0])**count
         
     def sample(self, M):
-        ...
+        tokens_sample = np.random.choice(self.mdl.index, replace=True, size=M)
+        tokens_sample = ' '.join(tokens_sample)
+        return tokens_sample
 
 
 # ---------------------------------------------------------------------
@@ -55,19 +96,32 @@ class UniformLM(object):
 
 
 class UnigramLM(object):
-    
+
     def __init__(self, tokens):
 
         self.mdl = self.train(tokens)
-    
+        
     def train(self, tokens):
-        ...
+        tokens_series = pd.Series(tokens).value_counts()
+        tokens_series = tokens_series / tokens_series.sum()
+        return tokens_series
     
     def probability(self, words):
-        ...
+        probs = []
+        for word in words:
+            if word in (self.mdl.index.to_list()):
+                probs += self.mdl.loc[word]
+        if len(probs) == 0:
+            return 0
+        sample_prob = probs[0]
+        for probs in probs[1:]: 
+            sample_prob *= prob
+        return sample_prob
         
     def sample(self, M):
-        ...
+        tokens_sample = np.random.choice(self.mdl.index, replace=True, size=M, p=self.mdl.values)
+        tokens_sample = ' '.join(tokens_sample)
+        return tokens_sample
 
 
 # ---------------------------------------------------------------------
